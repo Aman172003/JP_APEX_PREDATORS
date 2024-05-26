12f8866c97e012e32a64fe5bc6bfcd5cc1df5cdb
Aman Kumar Rai <tej171995@gmail.com>
2024-05-26 11:37:31 +0530

    Initial Commit

---
+++ JP_APEX_PREDATORS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JP_APEX_PREDATORS
+//
+//  Created by Aman on 26/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JP_APEX_PREDATORS/JP_APEX_PREDATORSApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_APEX_PREDATORSApp.swift
+//  JP_APEX_PREDATORS
+//
+//  Created by Aman on 26/05/24.
+//
+
+import SwiftUI
+
+@main
+struct JP_APEX_PREDATORSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
